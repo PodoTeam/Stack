d0c7d8080c8505ebe4eb9a85457c354dad83e41a
Isaac Haro <74152544+ieharo@users.noreply.github.com>
2020-11-30 08:28:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Pilas001
+{
+
+ 
+        class Pila
+        {
+            private int[] elemento = new int[20];
+            private int tope;
+
+            public Pila()
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    elemento[i] = 0;
+                    tope = -1;
+                }
+            }
+            public void ImprimirPila()
+            {
+
+                for (int i = 0; i <= tope; i++)
+                {
+                    Console.WriteLine(elemento[i]);
+                }
+                Console.WriteLine("Esto es todo!");
+            }
+            public void Push(int a)
+            {
+                elemento[tope + 1] = a;
+                tope += 1;
+            }
+            public int Pop()
+            {
+                int auxiliar;
+                auxiliar = elemento[tope];
+                elemento[tope] = 0;
+                tope -= 1;
+                return auxiliar;
+            }
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Pila Mi_Pila = new Pila();
+            Mi_Pila.Push(10);
+            Mi_Pila.ImprimirPila();
+        }
+    }
+}
